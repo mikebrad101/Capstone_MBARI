87a9b0798967a678e9f1c903fb7bd896e0675ac3
mikebrad101 <mrmikebrad101@gmail.com>
2023-10-13 13:30:54 -0700

    testing post

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const { executeSQL } = require('../controllers/sql.js');
-const app = express();
+const bodyParser = require('body-parser');
+router.use(bodyParser.json());
 
 router.get("/", async function(req, res) {
   res.render('home');
@@ -37,5 +38,31 @@ router.get("/dbTest", async function(req, res) {
   let rows = await executeSQL(sql); // Assuming executeSQL is a function to execute SQL queries
   res.send(rows);
 });
+
+router.post('/addPrecruise', async (req, res) => {
+  let ship_name = req.body.shipName;
+  let purpose = req.body.purpose;
+  let chief_scientist = req.body.chiefScientist;
+  let principal_investigator = req.body.principalInvestigator;
+  let sch_start = req.body.scheduledStartDatetime;
+  let sch_end = req.body.scheduledEndDatetime;
+  let equipmentDescription = req.body.scheduledStartDatetime;
+  let participants = req.body.participants;
+  let region_description = req.body.regionDescription;
+  let planned_track_description = req.body.plannedTrackDescription;
+  //const { ship_name, purpose, chief_scientist, principal_investigator, sch_start, sch_end, equipmentDescription, participants, region_description, planned_track_description } = req.body;
+  // Insert the new post into the database
+  let sql = 'INSERT INTO expedition (ship_name, purpose, chief_scientist, principal_investigator, sch_start, sch_end, equip_description, participants, region_description, planned_track_description) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+  await executeSQL(sql, [ship_name, purpose, chief_scientist, principal_investigator, sch_start, sch_end, equipmentDescription, participants, region_description, planned_track_description], (err, result) => {
+    if (err) {
+      console.error('Error inserting expedition: ' + err.message);
+      res.status(500).send('Error inserting expedition');
+      return;
+    }
+    res.status(201).json({ message: 'Expedition created successfully' });
+  });
+});
+
+module.exports = router;
 
 module.exports = router;
