f36e4aafe4e996bc5b56059e3c83dad1da49af0a
mikebrad101 <mrmikebrad101@gmail.com>
2023-10-27 11:49:59 -0700

    removed bad sql tests

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -69,22 +69,11 @@ router.get("/allcruises", async function(req, res) {
   res.render('allcruises', { "rows": rows});
 });
 
-router.get("/testSQL", async function(req, res) {
-  //in route we get sql statement and data
-  //then send it to the view using render
-  res.render('testSQL');
-});
 
 router.get("/getLastEntry", async function(req, res) {
   let sql = 'SELECT * FROM expedition ORDER BY expedition_ID DESC LIMIT 1;';
   let rows = await executeSQL(sql);
   res.send(rows);
-});
-
-router.post("/testSQLinsert", async function(req, res) {
-  let sql = 'INSERT INTO expedition (ship_name, purpose,chief_scientist,principal_investigator, sch_start, sch_end, equip_description, participants, region_description, planned_track_description, actual_start, actual_end, accomplishments, scientist_comments, operator_comments, sci_objective_met, equipment_function, other_comments, updated_by ) VALUES(\'TESTT I\', \'TEST Research\', \'Dr. TEST Johnson\', \'Prof. TEST Davis\', \'2023-01-10 08:00:00\', \'2023-01-20 16:00:00\', \'ROV, TEST\', \'Team of TESTS\', \'North TEST\', \'Exploration of underwater TESTS\', \'2023-01-12 08:30:00\', \'2023-01-19 15:45:00\', \'Discovered new TEST of marine life\', \'Impressive TEST formations\', \'Minor TEST issues\', TRUE, TRUE, \'Great TESTS\', \'001\');'
-  await executeSQL(sql);
-  res.redirect('/getLastEntry');
 });
 
 router.post("/addPrecruise", async function(req, res) {
